e98636131c9db6726235b30c10070a37f4875450
kulaevsu <s_kulaev@bk.ru>
2023-02-27 01:42:13 +0700

    validate before update session

--- src/api/session/content-types/session/lifecycles.js
+++ src/api/session/content-types/session/lifecycles.js
@@ -19,4 +19,22 @@ module.exports = {
       });
     }
   },
+  async beforeUpdate(event) {
+    const {
+      data: { begin, end, equipment },
+      where: { id },
+    } = event.params;
+    const resValidation = sessionValidation.checkValidBeginAndEnd(begin, end);
+    if (!resValidation.isValid) {
+      throw new DetailedError(resValidation.message, { begin, end });
+    }
+    const sessions = await strapi
+      .service('api::session.session')
+      .getSessionsByEquipmentInRangeDate(equipment, begin, end);
+    if (sessions.length != 0 && sessions[0].id != id) {
+      throw new DetailedError('Selected time range is busy', {
+        busy: sessions[0],
+      });
+    }
+  },
 };
